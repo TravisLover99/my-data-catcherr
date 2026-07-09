606762984430cd41f0dcdb55ac673f5dc77a7987
xtrackergs <orindrbx@gmail.com>
2026-07-09 13:34:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
+
 app.post('/', (req, res) => {
-    console.log('--- GOT IT! ---', req.body);
+
+    console.log('--- GOT IT! ---');
+
+    console.log(req.body);
+
     res.sendStatus(200);
+
 });
-app.listen(process.env.PORT || 3000);
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server läuft');
+});
