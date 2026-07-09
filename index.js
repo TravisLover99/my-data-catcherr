b3463ea3569317d68d743002107d107226b83712
xtrackergs <orindrbx@gmail.com>
2026-07-09 18:13:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,41 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// POST-Endpunkt
-app.post('/data', (req, res) => {
-
-    console.log('--- GOT IT! ---');
-
-    console.log(req.body);
-
-    res.status(200).json({
-        success: true,
-        received: req.body
-    });
-
-});
-
-// Optional: Test im Browser
+// Testseite
 app.get('/', (req, res) => {
     res.send('Server läuft');
+});
+
+// Daten empfangen
+app.get('/data/:payload', (req, res) => {
+
+    try {
+
+        const payload = decodeURIComponent(req.params.payload);
+
+        const data = JSON.parse(
+            Buffer.from(payload, 'base64').toString('utf8')
+        );
+
+        console.log('--- GOT IT! ---');
+        console.log(data);
+
+        res.status(200).json({
+            success: true,
+            received: data
+        });
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(400).json({
+            success: false,
+            error: 'Ungültige Daten'
+        });
+
+    }
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
